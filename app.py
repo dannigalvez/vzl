efb595839a48be04dceb23cc09bb07104107d35a
dannigalvez <danielgalvez@simnet.is>
2024-04-12 14:03:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-# app.py
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return "Homepage"
 
-@app.route('/contact', methods=['GET'])
+@app.route('/contact')
 def contact():
     return "Contact page"
-    
-if __name__ == "__main__":
-    app.run(debug=True)
